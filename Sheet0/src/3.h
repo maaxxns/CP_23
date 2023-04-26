f10922bee5b641b9c5e3b07ac101772d45cb89e6
maaxxns <max.koch@tu-dortmund.de>
2023-04-26 23:16:30 +0200

    made some comments

---
+++ Sheet0/src/3.h
@@ -0,0 +1,10 @@
+#ifndef TWO_H
+#define TWO_H
+
+double sym_recursive_euler(double recursion_value_n, double recursion_value_n_1 ,double stepwidth);
+// symmmetrical euler scheme to solve differential equations
+
+double recursive_euler(double recursion_value ,double stepwidth);
+// euler scheme to solve differential equations
+
+#endif
