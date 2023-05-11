7fdf9add2ae9c7f2f9eead353f62c627c325e61a
maaxxns <max.koch@tu-dortmund.de>
2023-05-11 22:53:23 +0200

    added E pot to the plot

--- Sheet3/scripts/plots.py
+++ Sheet3/scripts/plots.py
@@ -4,9 +4,11 @@ import matplotlib.pyplot as plt
 data = np.genfromtxt('bin/b)set.dat', delimiter=',', comments="#")
 
 plt.figure()
-plt.plot(data[:,0], data[:, 2], label='Ekin') 
+plt.plot(data[:,0], data[:, 2], label='Ekin')
+plt.plot(data[:,0], data[:, 3], label='Epot') 
+plt.plot(data[:,0], data[:, 2] + data[:, 3], label='Ekin + Epot')
 plt.xlabel("t")
-plt.ylabel("Ekin")
+plt.ylabel("Energie")
 plt.legend()
 plt.title("Ekin, h = 0.01")
 plt.tight_layout()
