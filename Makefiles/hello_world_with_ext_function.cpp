5d3505329ea12939ed97ea3094183a82df2d525b
smjolame <johannes.lamers@tu-dortmund.de>
2023-06-01 19:23:58 +0200

    Bla

---
+++ Makefiles/hello_world_with_ext_function.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "ext_function.hpp"
+
+int main() {
+    std::cout << "Hello from main!" << std::endl;
+
+    say_hello();
+    return 0;
+}
--- Zettel/Blatt_6/src/Ex1.cpp
+++ Zettel/Blatt_6/src/Ex1.cpp
@@ -8,27 +8,30 @@ using namespace std;
 class Bifurkationsdiagramm{
     public:
     Bifurkationsdiagramm(double);
-    double tolerance = 0.0001; // tolerance of beeing "equal"
+    double tolerance = 0.01; // tolerance of beeing "equal"
     double x_0;
     double r_max_log = 4;
     double r_max_kub = 3;
     double Delta_r = 0.001;
     unsigned int count = 0;
     double N;
+    vector<double> r_inf;
 
 
 
 
-    void logistisch_warm(double r);
+    
     int count_fix(double r);
-    void save_fixpoints(double r_max);
+    void save_fixpoints_log(double r_max);
+    void save_fixpoints_kub(double r_max);
 
     private: 
         double x;
 
         vector<double> fix_point;
+        void logistisch_warm(double r, double x_0);
 
-        void kubisch_warm(double r);
+        void kubisch_warm(double r, double x_0);
         
         
 
@@ -41,14 +44,14 @@ Bifurkationsdiagramm::Bifurkationsdiagramm(double x_0): x_0(x_0)
 
 }
 
-void Bifurkationsdiagramm::logistisch_warm(double r){
+void Bifurkationsdiagramm::logistisch_warm(double r,double x_0){
     x = x_0;
     for(unsigned int i = 0; i < N; i++){
         x = r*x*(1-x);
     }
 }
 
-void Bifurkationsdiagramm::kubisch_warm(double r){
+void Bifurkationsdiagramm::kubisch_warm(double r, double x_0){
     x = x_0;
     for(unsigned int i = 0; i < N; i++){
         x = r*x - x*x*x;
@@ -67,49 +70,78 @@ int Bifurkationsdiagramm::count_fix(double r){
     return count;
 }
 
-void Bifurkationsdiagramm::save_fixpoints(double r_max){
-    
-    ofstream r_data_csv ("build/r_data_csv.csv");
+void Bifurkationsdiagramm::save_fixpoints_log(double r_max){
+    vector<double> x_0 = {0.1,0.3,0.5,0.7,0.9};
+    ofstream r_inf_data("./build/r_inf_log.csv");
+    ofstream r_double_data("./build/r_double_log.csv");
 
-    count = 0;
-    int n = 0;
+    for(int i=0; i<size(x_0);i++){
+        ofstream r_data_csv("./build/r_data_log"+to_string(i)+".csv");
 
-    while(count <= 64){
-        fix_point = {};
-        double r = Delta_r * n;
         count = 0;
-        logistisch_warm(r);
-        do{
-        fix_point.push_back(x);
-        x = r*x*(1-x);
-        r_data_csv << r << ',' << x << endl;
-        count++;
-        n++;
-        }while(!(x-tolerance < fix_point[0] && fix_point[0] < x+tolerance));
+        int n = 0;
+
+        while(count <= 64){
+            fix_point = {};
+            double r = Delta_r * n;
+            count = 0;
+            logistisch_warm(r,x_0[i]);
+            do{
+                fix_point.push_back(x);
+                x = r*x*(1-x);
+                r_data_csv << r << "," << x << endl;
+                count++;
+                n++;
+                if(count > 64){
+                    r_inf_data << x_0[i] << "," << r << endl;
+                    break;
+                }
+                
+            }while(!(x-tolerance < fix_point[0] && fix_point[0] < x+tolerance));
+            
+        }
+        
         r_data_csv.close();
-
     }
-
-
-
+    r_inf_data.close();
+    r_double_data.close();
 
 }
 
-    
+void Bifurkationsdiagramm::save_fixpoints_kub(double r_max){
+    vector<double> x_0 = {0.1,0.3,0.5,0.7,0.9};
+    ofstream r_inf_data("./build/r_inf_kub.csv");
+    ofstream r_double_data("./build/r_double_kub.csv");
 
+    for(int i=0; i<size(x_0);i++){
+        ofstream r_data_csv("./build/r_data_kub"+to_string(i)+".csv");
 
+        count = 0;
+        int n = 0;
 
-//void Bifurkationsdiagramm::count_fix(){
-//    unsigned int i = 0; 
-//
-//    do{
-//        if(!(x-tolerance < fix_point[i][0] < x+tolerance)){
-//            fix_point.push_back({x,fix_point[i][1]+1});
-//            i++;
-//        }
-//        x = r*x*(1-x);
-//    }while(fix_point[i-1][1] <= 10);
-//}
+        while(count <= 64){
+            fix_point = {};
+            double r = Delta_r * n;
+            count = 0;
+            kubisch_warm(r,x_0[i]);
+            do{
+                fix_point.push_back(x);
+                x = r*x - x*x*x;
+                r_data_csv << r << "," << x << endl;
+                count++;
+                n++;
+                if(count > 64){
+                    r_inf_data << x_0[i] << "," << r << endl;
+                    break;
+                }
+            }while(!(x-tolerance < fix_point[0] && fix_point[0] < x+tolerance));
+        }
+        r_data_csv.close();
+    }
+    r_inf_data.close();
+    r_double_data.close();
+
+}
 
 int main(){
 
@@ -120,7 +152,11 @@ int main(){
 
     Bifurkationsdiagramm logistisch(x_0);
     logistisch.N = 30;
-    logistisch.save_fixpoints(4);
+    logistisch.save_fixpoints_log(4);
+
+    Bifurkationsdiagramm kubisch(x_0);
+    kubisch.N = 30;
+    kubisch.save_fixpoints_kub(3);
     
 
 
