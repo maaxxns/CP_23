c0fcfcfd97dfbb2ce37141a649803f6467b04301
smjolame <johannes.lamers@tu-dortmund.de>
2023-06-16 19:22:55 +0200

    Latex

--- Zettel/Blatt_8/scripts/plot.py
+++ Zettel/Blatt_8/scripts/plot.py
@@ -12,6 +12,8 @@ d = np.genfromtxt("./build/d_data.csv", unpack=True)
 
 
 Nr = np.array(["a","b","c","d"])
+Nr1 = np.array(["a","b"])
+Nr2 = np.array(["c","d"])
 
 for i in Nr:
     plt.hist(globals()[i],10, label = "Histogram " + i +")")
@@ -21,7 +23,7 @@ for i in Nr:
 
 
 
-for i in Nr:
+for i in Nr1:
     globals()[i+"_x"] = globals()[i][0:-2:2]
     globals()[i+"_y"] = globals()[i][1:-1:2]
 
@@ -30,3 +32,14 @@ for i in Nr:
     plt.title("Scatterplot of "+i+")")
     plt.savefig("./build/"+i+"2d.pdf")
     plt.clf()
+
+for i in Nr2:
+    globals()[i+"_x"] = globals()[i][0:-2:2]
+    globals()[i+"_y"] = globals()[i][1:-1:2]
+
+
+    plt.scatter(globals()[i+"_x"][::10],globals()[i+"_y"][::10], alpha=0.4, s = 1)
+    plt.title("Scatterplot of "+i+")")
+    plt.savefig("./build/"+i+"2d.pdf")
+    plt.clf()
+
