783f7951bb827ee6c29e340bb1111deb3a4f7f5c
Johannes Lamers <smjolame@tu-dortmund.de>
2023-07-03 10:53:51 +0200

    B10

--- Zettel/Blatt_10/src/Ex1.cpp
+++ Zettel/Blatt_10/src/Ex1.cpp
@@ -6,9 +6,29 @@
 using namespace std;
 
 
+double Hamilton(int& sigma, double& H){
+    return sigma * H;
+}
+
+class MC{
+    private:
+    MC(double (*Hamilton)(int, double));
+    void equilibrate();
+    public:
+    
+};
 
 
 int main(){
+
+    double V = 0.5;
+    int steps = 10;
+    double H = 2.0;
+
+    for(int t = 0; t< steps; t++){
+        int sigma = 1;
+        double Ham = Hamilton(sigma, H);
+    }
 
 
     return 0;
