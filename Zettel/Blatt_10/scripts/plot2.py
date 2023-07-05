9c8044265b2e704fa11f6138967efd0640e0c441
smjolame <johannes.lamers@tu-dortmund.de>
2023-07-05 19:24:56 +0200

    Bla

--- Zettel/Blatt_10/scripts/plot2.py
+++ Zettel/Blatt_10/scripts/plot2.py
@@ -1,11 +1,24 @@
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
 import numpy as np
+import matplotlib.colors as mcolors
 
 #get from: ./build/data.csv
 #save in: ./build/Graph.pdf
 
 
+def create_color_map_from_csv(k_BT, Nr):
+    data = np.loadtxt("./build/k_BT:"+str(k_BT)+"_sweep:"+str(Nr)+".csv", delimiter=',')
+    cmap = mcolors.ListedColormap(['blue', 'red'])
+    plt.imshow(data, cmap=cmap)
+    plt.colorbar(ticks=[-1,1])
+    plt.savefig("./build/Ising"+str(k_BT)+"_"+str(Nr)+".pdf")
+    plt.title("k_BT:"+str(k_BT)+", Sweep:"+str(Nr*2e3))
+    plt.tight_layout()
+    plt.clf()
+
+for k in np.array([1,3]):
+    for i in np.array([0,1,2,3,4]):
+        create_color_map_from_csv(k,i)
 
 
 
--- Zettel/Blatt_10/src/Ex2.cpp
+++ Zettel/Blatt_10/src/Ex2.cpp
@@ -6,36 +6,52 @@
 #include <random>
 using namespace std;
 
-template<unsigned int size>
+template<int size>
 class Ising_Metropolis{
-    double k_BT = 1;
-    double beta = 1/k_BT;
-    mt19937 rnd;
+    private:
+        random_device rd;
+        mt19937 generator;
+        uniform_real_distribution<double> p;
+        uniform_int_distribution<> dist_i, dist_j;
+
+        void saveToCSV(const std::string& filename);
+        double Energy(int, int, int);
     public:
-    Ising_Metropolis();
-
-    int Spins[size][size];
-
-    void initialise_rnd();
-    void initialise_orderly();
-    void equlibrate();
-    double Energy(int, int, int);
+        Ising_Metropolis() 
+        : 
+            generator(rd()),
+            p(0,1),
+            dist_i(0,size-1),
+            dist_j(0,size-1)
+        {};
+        double k_BT = 1;
+        double beta = 1/k_BT;
+        int Spins[size][size];
+        void set_k_BT(double);
+        void initialise_rnd();
+        void initialise_orderly();
+        void equilibrate(int);
+        void measure(int, int);
+        
     
 
 };
-template <unsigned int size> Ising_Metropolis<size>::Ising_Metropolis(){
-
-};
-template <unsigned int size> void Ising_Metropolis<size>::initialise_rnd(){
-    uniform_real_distribution<double> dist(0, 1);
+template <int size> void Ising_Metropolis<size>::initialise_rnd(){
     for(int i=0; i < size; i++){
         for(int j=0; j < size; j++){
-            Spins[i][j] = (dist(rnd) < 0.5) ? -1 : 1;
+            Spins[i][j] = (p(generator) < 0.5) ? -1 : 1;
         }
 
     }
 };
-template <unsigned int size> void Ising_Metropolis<size>::initialise_orderly(){
+
+template <int size> void Ising_Metropolis<size>::set_k_BT(double new_k_BT){
+    k_BT = new_k_BT;
+    beta = 1/k_BT;
+
+}
+
+template <int size> void Ising_Metropolis<size>::initialise_orderly(){
     for(int i=0; i < size; i++){
         for(int j=0; j < size; j++){
             Spins[i][j] = 1;
@@ -44,27 +60,57 @@ template <unsigned int size> void Ising_Metropolis<size>::initialise_orderly(){
     }
 };
 
-template <unsigned int size> void Ising_Metropolis<size>::equlibrate(){
+template <int size> void Ising_Metropolis<size>::equilibrate(int n_sweeps){
     
-    uniform_real_distribution<int> dist_i(0, size-1);
-    uniform_real_distribution<int> dist_j(0, size-1);
-    int i = dist_i(rnd);
-    int j = dist_j(rnd);
-    int spin = Spins[i][j];
-    int old_spin = spin;
-    spin *= -1;
-    double Delta_E = Energy(i,j, spin) - Energy(i,j, old_spin);
-    if(Delta_E < 0 || dist(rnd) < exp(-beta*Delta_E)){
-        magnetisation_sum += spin;
+    for(int sweeps = 0; sweeps < n_sweeps; sweeps++){
+        for(int iterator = 0; iterator < size*size; iterator++){
+            int i = dist_i(generator);
+            int j = dist_j(generator);
+            int spin = Spins[i][j];
+            int old_spin = spin;
+            spin *= -1;
+            double Delta_E = Energy(i,j, spin) - Energy(i,j, old_spin);
+            if(Delta_E < 0 || p(generator) < exp(-beta*Delta_E)){       
+                Spins[i][j] = spin;
+            }
+            else{
+                Spins[i][j] = old_spin;
+    
+            }
+        }
     }
-    else{
-        spin = old_spin;
-        magnetisation_sum += spin;
-    }
-
-
 };
-template <unsigned int size> double Ising_Metropolis<size>::Energy(int i, int j, int spin_loc){
+
+template <int size> void Ising_Metropolis<size>::measure(int n_equilibrate, int n_sweeps){
+
+    equilibrate(n_equilibrate);
+    for(int sweeps = 0; sweeps < n_sweeps; sweeps++){
+        for(int iterator = 0; iterator < size*size; iterator++){
+            int i = dist_i(generator);
+            int j = dist_j(generator);
+            int spin = Spins[i][j];
+            int old_spin = spin;
+            spin *= -1;
+            double Delta_E = Energy(i,j, spin) - Energy(i,j, old_spin);
+            if(Delta_E < 0 || p(generator) < exp(-beta*Delta_E)){       
+                Spins[i][j] = spin;
+            }
+            else{
+                Spins[i][j] = old_spin;
+    
+            }
+        }
+        if(sweeps%(n_sweeps/5)==0){
+            saveToCSV("k_BT:"+to_string(int(k_BT))+"_sweep:"+to_string(sweeps/(n_sweeps/5)));
+        }
+
+    
+    }
+};
+
+
+
+template <int size> double Ising_Metropolis<size>::Energy(int i, int j, int spin_loc){
     int energy_sum;
     int spin_top; 
     int spin_down; 
@@ -106,17 +152,36 @@ template <unsigned int size> double Ising_Metropolis<size>::Energy(int i, int j,
 }
 
 
+template<int size> void Ising_Metropolis<size>::saveToCSV(const std::string& filename) {
+    std::ofstream outputFile("build/"+filename+".csv");
+
+    if (outputFile.is_open()) {
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
+                outputFile << Spins[i][j];
+                if (j != size - 1) {
+                    outputFile << ",";
+                }
+            }
+            outputFile << "\n";
+        }
+
+        outputFile.close();
+        std::cout << "CSV file saved successfully.\n";
+    } else {
+        std::cout << "Unable to open the file.\n";
+    }
+};
+
 int main(){
 
-
-Ising_Metropolis<10> Test1;
+int const size = 100;
+Ising_Metropolis<size> Test1;
 Test1.initialise_rnd();
-for(int i = 0; i<10; i++){
-    for(int j = 0; j<10; j++){
-            cout << i << ','<< j << ',' << Test1.Energy(i,j) << endl;
-    }
+Test1.measure(100,1e4);
+Test1.set_k_BT(3.0);
+Test1.measure(100,1e4);
 
-}
 
 
 
